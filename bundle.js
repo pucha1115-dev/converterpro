83f525d912b61523e0edc5c972442732a92f8f9b
jp-bns2020 <71251589+pucha1115-dev@users.noreply.github.com>
2024-04-17 16:34:57 +0800

    Add files via upload

---
+++ bundle.js
@@ -0,0 +1,96 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./country-list.js":
+/*!*************************!*\
+  !*** ./country-list.js ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst country_list = {\r\n  AED: \"AE\",\r\n  AFN: \"AF\",\r\n  XCD: \"AG\",\r\n  ALL: \"AL\",\r\n  AMD: \"AM\",\r\n  ANG: \"AN\",\r\n  AOA: \"AO\",\r\n  AQD: \"AQ\",\r\n  ARS: \"AR\",\r\n  AUD: \"AU\",\r\n  AZN: \"AZ\",\r\n  BAM: \"BA\",\r\n  BBD: \"BB\",\r\n  BDT: \"BD\",\r\n  XOF: \"BE\",\r\n  BGN: \"BG\",\r\n  BHD: \"BH\",\r\n  BIF: \"BI\",\r\n  BMD: \"BM\",\r\n  BND: \"BN\",\r\n  BOB: \"BO\",\r\n  BRL: \"BR\",\r\n  BSD: \"BS\",\r\n  NOK: \"BV\",\r\n  BWP: \"BW\",\r\n  BYR: \"BY\",\r\n  BZD: \"BZ\",\r\n  CAD: \"CA\",\r\n  CDF: \"CD\",\r\n  XAF: \"CF\",\r\n  CHF: \"CH\",\r\n  CLP: \"CL\",\r\n  CNY: \"CN\",\r\n  COP: \"CO\",\r\n  CRC: \"CR\",\r\n  CUP: \"CU\",\r\n  CVE: \"CV\",\r\n  CYP: \"CY\",\r\n  CZK: \"CZ\",\r\n  DJF: \"DJ\",\r\n  DKK: \"DK\",\r\n  DOP: \"DO\",\r\n  DZD: \"DZ\",\r\n  ECS: \"EC\",\r\n  EEK: \"EE\",\r\n  EGP: \"EG\",\r\n  ETB: \"ET\",\r\n  EUR: \"FR\",\r\n  FJD: \"FJ\",\r\n  FKP: \"FK\",\r\n  GBP: \"GB\",\r\n  GEL: \"GE\",\r\n  GGP: \"GG\",\r\n  GHS: \"GH\",\r\n  GIP: \"GI\",\r\n  GMD: \"GM\",\r\n  GNF: \"GN\",\r\n  GTQ: \"GT\",\r\n  GYD: \"GY\",\r\n  HKD: \"HK\",\r\n  HNL: \"HN\",\r\n  HRK: \"HR\",\r\n  HTG: \"HT\",\r\n  HUF: \"HU\",\r\n  IDR: \"ID\",\r\n  ILS: \"IL\",\r\n  INR: \"IN\",\r\n  IQD: \"IQ\",\r\n  IRR: \"IR\",\r\n  ISK: \"IS\",\r\n  JMD: \"JM\",\r\n  JOD: \"JO\",\r\n  JPY: \"JP\",\r\n  KES: \"KE\",\r\n  KGS: \"KG\",\r\n  KHR: \"KH\",\r\n  KMF: \"KM\",\r\n  KPW: \"KP\",\r\n  KRW: \"KR\",\r\n  KWD: \"KW\",\r\n  KYD: \"KY\",\r\n  KZT: \"KZ\",\r\n  LAK: \"LA\",\r\n  LBP: \"LB\",\r\n  LKR: \"LK\",\r\n  LRD: \"LR\",\r\n  LSL: \"LS\",\r\n  LTL: \"LT\",\r\n  LVL: \"LV\",\r\n  LYD: \"LY\",\r\n  MAD: \"MA\",\r\n  MDL: \"MD\",\r\n  MGA: \"MG\",\r\n  MKD: \"MK\",\r\n  MMK: \"MM\",\r\n  MNT: \"MN\",\r\n  MOP: \"MO\",\r\n  MRO: \"MR\",\r\n  MTL: \"MT\",\r\n  MUR: \"MU\",\r\n  MVR: \"MV\",\r\n  MWK: \"MW\",\r\n  MXN: \"MX\",\r\n  MYR: \"MY\",\r\n  MZN: \"MZ\",\r\n  NAD: \"NA\",\r\n  XPF: \"NC\",\r\n  NGN: \"NG\",\r\n  NIO: \"NI\",\r\n  NPR: \"NP\",\r\n  NZD: \"NZ\",\r\n  OMR: \"OM\",\r\n  PAB: \"PA\",\r\n  PEN: \"PE\",\r\n  PGK: \"PG\",\r\n  PHP: \"PH\",\r\n  PKR: \"PK\",\r\n  PLN: \"PL\",\r\n  PYG: \"PY\",\r\n  QAR: \"QA\",\r\n  RON: \"RO\",\r\n  RSD: \"RS\",\r\n  RUB: \"RU\",\r\n  RWF: \"RW\",\r\n  SAR: \"SA\",\r\n  SBD: \"SB\",\r\n  SCR: \"SC\",\r\n  SDG: \"SD\",\r\n  SEK: \"SE\",\r\n  SGD: \"SG\",\r\n  SKK: \"SK\",\r\n  SLL: \"SL\",\r\n  SOS: \"SO\",\r\n  SRD: \"SR\",\r\n  STD: \"ST\",\r\n  SVC: \"SV\",\r\n  SYP: \"SY\",\r\n  SZL: \"SZ\",\r\n  THB: \"TH\",\r\n  TJS: \"TJ\",\r\n  TMT: \"TM\",\r\n  TND: \"TN\",\r\n  TOP: \"TO\",\r\n  TRY: \"TR\",\r\n  TTD: \"TT\",\r\n  TWD: \"TW\",\r\n  TZS: \"TZ\",\r\n  UAH: \"UA\",\r\n  UGX: \"UG\",\r\n  USD: \"US\",\r\n  UYU: \"UY\",\r\n  UZS: \"UZ\",\r\n  VEF: \"VE\",\r\n  VND: \"VN\",\r\n  VUV: \"VU\",\r\n  YER: \"YE\",\r\n  ZAR: \"ZA\",\r\n  ZMK: \"ZM\",\r\n  ZWD: \"ZW\",\r\n};\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (country_list);\r\n\n\n//# sourceURL=webpack://currency-converter/./country-list.js?");
+
+/***/ }),
+
+/***/ "./script.js":
+/*!*******************!*\
+  !*** ./script.js ***!
+  \*******************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _country_list__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./country-list */ \"./country-list.js\");\nconst dropList = document.querySelectorAll(\".drop-list select\"),\r\n  fromCurrency = document.querySelector(\".from select\"),\r\n  toCurrency = document.querySelector(\".to select\"),\r\n  getButton = document.querySelector(\"form button\");\r\nconst apiKey = \"412d60f7ff33d6f4c741acb0\";\r\nconst ipinfo_token = \"89084a7359ef45\";\r\n\r\n\r\nfor (let i = 0; i < dropList.length; i++) {\r\n  for (let currency_code in _country_list__WEBPACK_IMPORTED_MODULE_0__[\"default\"]) {\r\n    // selecting USD by default as FROm currency and NPR as TO currency\r\n    let selected;\r\n    if (i == 0) {\r\n      selected = currency_code == \"USD\" ? \"selected\" : \"\";\r\n    } else if (i == 1) {\r\n      selected = currency_code == \"PHP\" ? \"selected\" : \"\";\r\n    }\r\n    // creating option tag with passing currency code as a text and value\r\n    let optionTag = `<option value=\"${currency_code}\" ${selected}>${currency_code}</option>`;\r\n    // insert options tag inside select tag\r\n    dropList[i].insertAdjacentHTML(\"beforeend\", optionTag);\r\n  }\r\n  dropList[i].addEventListener(\"change\", (e) => {\r\n    loadFlag(e.target); //calling loadFLag with passing target element as argument\r\n  });\r\n}\r\n\r\nfunction loadFlag(element) {\r\n  for (let country_code in _country_list__WEBPACK_IMPORTED_MODULE_0__[\"default\"]) {\r\n    if (country_code == element.value) {\r\n      // if currency code of country list is equal to option value\r\n      let imgTag = element.parentElement.querySelector(\"img\"); //selecting img tag of particular drop list\r\n      // passing country code of a selected curreny code in an img url\r\n      imgTag.src = `https://flagsapi.com/${_country_list__WEBPACK_IMPORTED_MODULE_0__[\"default\"][country_code]}/flat/64.png`;\r\n    }\r\n  }\r\n}\r\n\r\nwindow.addEventListener(\"load\", () => {\r\n  //run getExchangeRate() on load\r\n  getExchangeRate();\r\n  detectCountry();\r\n  getJson();\r\n});\r\n\r\ngetButton.addEventListener(\"click\", (e) => {\r\n  e.preventDefault(); //preventing form from submitting and refresh\r\n  getExchangeRate();\r\n});\r\n\r\nconst exchangeIcon = document.querySelector(\".drop-list .icon\");\r\nexchangeIcon.addEventListener(\"click\", () => {\r\n  let tempCode = fromCurrency.value; // temporary currency code from drop list\r\n  fromCurrency.value = toCurrency.value; // pass TO currency code to From currency code\r\n  toCurrency.value = tempCode; // passing temporay currency code to TO currency code\r\n  loadFlag(fromCurrency); // calling loadFlag with passing select element (fromCurrency) of FROm\r\n  loadFlag(toCurrency); // calling loadFlag with passing select element (toCurrent) of TO\r\n  getExchangeRate(); // run exchange rate after changing\r\n});\r\n\r\nfunction getExchangeRate() {\r\n  checkValidInput();\r\n  const amount = document.querySelector(\".amount input\");\r\n  var exchangeRateText = document.querySelector(\".exchange-rate\");\r\n  let amountVal = amount.value;\r\n  if (amountVal == \"\" || amountVal == \"0\") {\r\n    amount.value = \"1\";\r\n    amountVal = 1;\r\n  }\r\n  exchangeRateText.textContent = \"Getting exchange rate...\";\r\n  let url = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${fromCurrency.value}`;\r\n  fetch(url)\r\n    .then((response) => response.json())\r\n    .then((result) => {\r\n      let exchangeRate = result.conversion_rates[toCurrency.value];\r\n      let totalExhangeRate = (amountVal * exchangeRate).toFixed(2);\r\n      if (isNaN(totalExhangeRate) || totalExhangeRate < 0) {\r\n        exchangeRateText.textContent = \"Invalid Input\";\r\n      } else\r\n        exchangeRateText.textContent = `${amountVal} ${fromCurrency.value} = ${totalExhangeRate} ${toCurrency.value}`;\r\n    })\r\n    .catch(() => {\r\n      exchangeRateText.textContent =\r\n        \"Someting went wrong. Please try again later.\";\r\n    });\r\n}\r\n\r\nfunction checkValidInput() {\r\n  var errorMessage = document.querySelector(\".error-message\");\r\n  var input = document.querySelector(\".amount input\");\r\n\r\n  errorMessage.textContent = \"\";\r\n\r\n  if (isNaN(input.value) || parseFloat(input.value) < 0) {\r\n    errorMessage.textContent = \"Please enter valid amount.\";\r\n  }\r\n}\r\n\r\nfunction detectCountry() {\r\n  var client_location = document.querySelector(\".current_location\");\r\n  client_location.textContent = \"\";\r\n\r\n  fetch(`https://ipinfo.io/json?token=${ipinfo_token}`) // Replace 'YOUR_TOKEN' with your API token from ipinfo.io\r\n    .then((response) => response.json())\r\n    .then((data) => {\r\n      var country = data.country;\r\n      const current_currency = getCurrency(country);\r\n      client_location.textContent = \"Your Currency: \" + current_currency;\r\n    })\r\n    .catch((error) => console.log(error));\r\n}\r\n\r\nfunction getCurrency(country) {\r\n  for (let currency_code in _country_list__WEBPACK_IMPORTED_MODULE_0__[\"default\"]) {\r\n    if (_country_list__WEBPACK_IMPORTED_MODULE_0__[\"default\"].hasOwnProperty(currency_code)) {\r\n      if (_country_list__WEBPACK_IMPORTED_MODULE_0__[\"default\"][currency_code] === country) {\r\n        return currency_code; // if found this returns the currency\r\n      }\r\n    }\r\n  }\r\n  return \"USD\"; // if not found returns USD as default currency\r\n}\r\n\r\nfunction getJson() {\r\n  var priceElement = document.querySelector(\".price\");\r\n\r\n  fetch(\"price.json\")\r\n    .then((response) => response.json())\r\n    .then((data) => {\r\n      console.log(\"Current price:\", data.price);\r\n      // Save the current price to compare with the new price later\r\n      var currentPrice = data.price;\r\n      priceElement.textContent = \"Price: \" + currentPrice;\r\n\r\n      // Poll the server every 5 seconds to check for updates\r\n      setInterval(() => {\r\n        console.log(\"Checking for updates...\");\r\n        fetch(\"price.json\")\r\n          .then((response) => response.json())\r\n          .then((newData) => {\r\n            console.log(\"New price:\", newData.price);\r\n            // Check if the price has changed\r\n            if (newData.price !== currentPrice) {\r\n              console.log(\"Price has changed. Updating...\");\r\n              currentPrice = newData.price;\r\n              priceElement.textContent = \"Price: \" + currentPrice; // Update the price\r\n            }\r\n          })\r\n          .catch((error) => {\r\n            console.error(\"Error fetching JSON:\", error);\r\n          });\r\n      }, 5000); // Poll every 5 seconds (adjust as needed)\r\n    })\r\n    .catch((error) => {\r\n      console.error(\"Error fetching JSON:\", error);\r\n    });\r\n}\r\n\n\n//# sourceURL=webpack://currency-converter/./script.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./script.js");
+/******/ 	
+/******/ })()
+;
